27809b1dcb643a6ee5a225f3b11db8c7c00bd68c
Hazrat Ali <iloveyii@ayahoo.com>
2024-10-12 15:33:14 +0200

    feat: charts

--- src/rparadise/graphics.r
+++ src/rparadise/graphics.r
@@ -1,6 +1,20 @@
 # The last open device becomes the active graphical device on which all subsequent graphs are displayed. 
 # The function dev.list() displays the list of open devices
-pdf()
-png()
+# pdf()
+# png()
+# svg()
+# dev.list()
+
+
+x <- rnorm(10, 300, 20)
+y <- rnorm(10, 3, 1)
 svg()
-dev.list()
+layout(matrix(1:6, 3, 2))
+layout.show(6)
+
+plot(x, y)
+x
+y
+pie(x)
+dotchart(y)
+
